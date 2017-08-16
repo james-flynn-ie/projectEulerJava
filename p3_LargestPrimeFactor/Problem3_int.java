94be43628e2bedbb9a21b346f9218ad17b6de65a
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2017-08-16 22:04:02 +0100

    Created new classes for filling ArrayLists.
    Signed-off-by: james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>

--- p3_LargestPrimeFactor/Problem3_int.java
+++ p3_LargestPrimeFactor/Problem3_int.java
@@ -7,11 +7,31 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * What is the largest prime factor of the number 600851475143 ?
  */
 public class Problem3_int {
-
     /**
      * This approach works for ints such as 13195, but is not efficient for long integrals.
      * A more efficient approach is defined in https://github.com/james-flynn-ie/projectEulerJava/blob/master/p3_LargestPrimeFactor/Problem3_long.java
      */
+    private static CopyOnWriteArrayList<Integer> getAllFactors (int numberUnderTest, CopyOnWriteArrayList<Integer> allFactors) {
+        for (int i = 2; i <= numberUnderTest; i++) {
+            if (numberUnderTest % i == 0) {
+                allFactors.add(i);
+            }
+        }
+        return allFactors;
+    }
+
+    private static CopyOnWriteArrayList<Integer> removePrimesFromAllFactors (CopyOnWriteArrayList<Integer> allFactors) {
+        for (int i = 0; i < allFactors.size(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (! allFactors.get(i).equals(allFactors.get(j)) && (allFactors.get(i) % allFactors.get(j) == 0)) {
+                    allFactors.remove(i);
+                    // Decrement 'i' so that we don't miss one of the factors after the post-removal reindexing in for-loop.
+                    i--;
+                }
+            }
+        }
+        return allFactors;
+    }
 
     public static void main(String[] args) {
         int numberUnderTest = 13_195;
@@ -19,22 +39,10 @@ public class Problem3_int {
         CopyOnWriteArrayList<Integer> allFactors = new CopyOnWriteArrayList<>();
 
         // Get list of all factors, whether they are prime or not.
-        for (int i = 2; i <= numberUnderTest; i++) {
-            if (numberUnderTest % i == 0) {
-                allFactors.add(i);
-            }
-        }
+        allFactors = getAllFactors(numberUnderTest, allFactors);
 
         // Check if any factors can be divided by another factor, i.e.: are not prime.
-        for (int i = 0; i < allFactors.size(); i++) {
-            for (int j = 0; j <= i; j++) {
-                if (! allFactors.get(i).equals(allFactors.get(j)) && (allFactors.get(i) % allFactors.get(j) == 0)) {
-                    allFactors.remove(i);
-                    // Decrement 'i' so that we don't miss one of the factors after the post-removal reindexing.
-                    i--;
-                }
-            }
-        }
+        allFactors = removePrimesFromAllFactors(allFactors);
 
         System.out.println("The prime factors for " + numberUnderTest + " are : " + allFactors);
     }
