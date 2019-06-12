ebeda1ade5d27b26803fcc46b0f355062f3ab292
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-06-12 18:19:32 +0100

    Reduced number of args passed by moving string type conversion to the other method

--- p4LargestPalindromeProduct/Problem4.java
+++ p4LargestPalindromeProduct/Problem4.java
@@ -9,11 +9,16 @@ public class Problem4 {
      * Find the largest palindrome made from the product of two 3-digit numbers.
      */
 
-    static int findLargestPalindrome(int result, String reverseResult, String strResult, int largestPalindrome) {
-        if ((reverseResult).equals(strResult)) {
-            // Uncomment this to see the full palindrome list. 
+    static int findLargestPalindrome(int result, int largestPalindrome) {
+        //Convert result to String and then reverse it.
+        String strResult = Integer.toString(result);
+        String reverseResult = new StringBuffer(strResult).reverse().toString();
+
+        if (reverseResult.equals(strResult)) {
+
+            // Uncomment the next line to see the full palindrome list:
             // System.out.println("Palindrome found! " + result);
-            
+
             if (result > largestPalindrome) {
                 largestPalindrome = result;
             }
@@ -28,14 +33,11 @@ public class Problem4 {
         for (int i = 100; i <= 999; i++) {
             for (int j = 100; j <= 999; j++) {
                 int result = i * j;
+
                 // Uncomment this println to see the output of each loop. Beware, it is verbose!
                 //System.out.println("(" + i + ") * (" + j + ") = " + result);
 
-                //Convert result to String before reversing.
-                String strResult = Integer.toString(result);
-                String reverseResult = new StringBuffer(strResult).reverse().toString();
-
-                largestPalindrome = findLargestPalindrome(result, strResult, reverseResult, largestPalindrome);
+                largestPalindrome = findLargestPalindrome(result, largestPalindrome);
             }
         }
 
