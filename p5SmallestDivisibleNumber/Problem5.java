56bca2288e1e31ea873e5e7d6ede29cec3f2ddb9
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-06-21 18:39:58 +0100

    Refactor: Improved comments

--- p5SmallestDivisibleNumber/Problem5.java
+++ p5SmallestDivisibleNumber/Problem5.java
@@ -11,7 +11,8 @@ package p5SmallestDivisibleNumber;
 public class Problem5 {
     final static int MAX_NUMBER = 20;
 
-    /* The Greatest Common Factor (GCF) is the largest positive integer 
+    /* 
+     * The Greatest Common Factor (GCF) is the largest positive integer 
      * that divides evenly into the numbers a and b with no remainder. 
      */
     static int gcf(int a, int b) {
@@ -24,27 +25,27 @@ public class Problem5 {
     }
 
     public static void main(String[] args) {
-        /* Since we are given in the question that the smallest divisible 
+        /* 
+         * Since we are given in the question that the smallest divisible 
          * number of 1 to 10 is 2520, and 10 is a factor of 20, 
          * we can start there!
          * 
-         * We set the result to 2520, and then start calculating 
-         * from 10 to 20. 
-         * 
-         * We could just as easily start at 1, but it would be slower.
+         * We could just as easily start at 1 (result = 1), but it would be 
+         * slower.
          */
         int result = 2520;
 
-        /* We find the Lowest Common Multiple (LCM), which can be defined as:
+        /* 
+         * We find the Lowest Common Multiple (LCM), which can be defined as:
          * "For two integers a and b, denoted LCM(a,b), the LCM is the smallest
          *  integer that is evenly divisible by both a and b"
          *  https://www.calculatorsoup.com/calculators/math/lcm.php
          * 
          * Using the Greatest Common Factor (GCF) approach, the formula is: 
-         *     LCM(a,b) = (a*b)/GCD(a,b)
+         *     LCM(a,b) = (a*b)/GCF(a,b)
          * 
          * We could also have used prime factorisation to calculate the LCM, 
-         * but we used already used that approach in Problem 3.
+         * but we already used that approach in Problem 3.
          */
         for (int i = 10; i <= MAX_NUMBER; i++) {
             int currentGcf = gcf(i, result);
