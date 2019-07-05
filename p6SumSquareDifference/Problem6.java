a7e0a0d1e524cba754bfa3c44a1ce0d487b1dc0b
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-05 17:49:36 +0100

    refactor: Adding comments for algorithms

--- p6SumSquareDifference/Problem6.java
+++ p6SumSquareDifference/Problem6.java
@@ -15,11 +15,19 @@ public class Problem6 {
     public static void main(String[] args) {
         final int N = 100;
 
-        int sumofnums = N * (N + 1) / 2;    
-        int sumofsquares = (N * (N + 1) * (2*N + 1)) / 6;     
-
+        /*
+	     * We can use the following Sum of Natural Numbers algorithm:
+	     * https: //trans4mind.com/personal_development/mathematics/series/sumNaturalNumbers.htm#mozTocId914933
+	     */
+        int sumofnums = N * (N + 1) / 2;
         int squaredsum = (int) Math.pow(sumofnums, 2);
         System.out.println("The squared sum of the squares of the first " + N + " natural numbers is: " + squaredsum);
+
+        /*
+	     * We can use the following Sum of the Squares of Natural Numbers algorithm:
+	     * https://trans4mind.com/personal_development/mathematics/series/sumNaturalSquares.htm
+	     */
+        int sumofsquares = (N * (N + 1) * (2*N + 1)) / 6;     
         System.out.println("The sum of the squares of the first " + N + " natural numbers is: " + sumofsquares);
 
         int difference = squaredsum - sumofsquares;
