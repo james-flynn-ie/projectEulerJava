906d54e564562556358f898d343a29725032c8ec
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-17 20:54:55 +0100

    Init commit solution 7

---
+++ p7TenThousandAndFirstPrimeNumber/Problem7.java
@@ -0,0 +1,7 @@
+package p7TenThousandAndFirstPrimeNumber;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        System.out.println("Problem 7 is a work in progress. Check back soon!");
+    }
+}
