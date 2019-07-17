eac14acc153b8a2cf34fdc3bb1c20fee3ee873f0
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-17 20:56:10 +0100

    Added javadoc comment for p7

--- p7TenThousandAndFirstPrimeNumber/Problem7.java
+++ p7TenThousandAndFirstPrimeNumber/Problem7.java
@@ -1,5 +1,11 @@
 package p7TenThousandAndFirstPrimeNumber;
 
+/**
+ * @author james-flynn-ie
+ * @see <a href="https://projecteuler.net/problem=7">https://projecteuler.net/problem=7</a>
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * What is the 10 001st prime number?
+ */
 public class Problem7 {
     public static void main(String[] args) {
         System.out.println("Problem 7 is a work in progress. Check back soon!");
