7789e3ffe58f876910f2d198978e7769a8fb0a81
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-21 13:58:10 +0100

    Refactor: p6 pkg name to lowercase

--- src/main/java/com/github/james_flynn_ie/projectEulerJava/p6SumSquareDifference/Problem6.java
+++ src/main/java/com/github/james_flynn_ie/projectEulerJava/p6SumSquareDifference/Problem6.java
@@ -1,4 +1,4 @@
-package src.main.java.com.github.james_flynn_ie.projecteulerjava.p6SumSquareDifference;
+package src.main.java.com.github.james_flynn_ie.projecteulerjava.p6sumsquaredifference;
 
 /**
  * @author james-flynn-ie
