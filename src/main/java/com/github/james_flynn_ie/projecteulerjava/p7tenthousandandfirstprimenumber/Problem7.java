42b7cd4362526658473282983a9505098cf91b05
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-27 08:02:14 +0100

    p7 solution

--- src/main/java/com/github/james_flynn_ie/projecteulerjava/p7tenthousandandfirstprimenumber/Problem7.java
+++ src/main/java/com/github/james_flynn_ie/projecteulerjava/p7tenthousandandfirstprimenumber/Problem7.java
@@ -7,7 +7,33 @@ package src.main.java.com.github.james_flynn_ie.projecteulerjava.p7tenthousandan
  * What is the 10 001st prime number?
  */
 public class Problem7 {
+    static final int MAX_PRIME_NUMBER = 10001;
+
+    // returns the nth prime number
+    public static long nthPrime(long n) {
+        int primesFound = 0;
+        long numberUnderTest = 1;
+        
+        while (primesFound < n) {
+            numberUnderTest++;
+            if (isPrime(numberUnderTest)) {
+                primesFound++;
+            }
+        }
+        return numberUnderTest;
+    }
+    
+    // returns true if parameter n is a prime number, false if composite or neither
+    public static boolean isPrime(long n) {
+        if (n < 2) return false;
+        else if (n == 2) return true;
+        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Problem 7 is a work in progress. Check back soon!");
+        System.out.println("The " + MAX_PRIME_NUMBER + " prime number is: " + nthPrime(MAX_PRIME_NUMBER));
     }
 }
